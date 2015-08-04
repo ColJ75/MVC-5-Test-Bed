023a1d3e32caf38dd0652f934c4fad99d99d7050
Colin Jaggs <colin@jaggs.co.uk>
2015-08-04 01:23:24 +0100

    Initial Commit

---
+++ MVC5 Test Bed/Controllers/TestController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5TestBed.Models;
+
+namespace MVC5TestBed.Controllers
+{
+	public class TestController : Controller
+	{
+		public ActionResult Index()
+		{
+			// create list of employee models
+			List<EmployeeView> employees = new List<EmployeeView>();
+			employees.Add(new EmployeeView((Employee)new Employee() { FirstName = "Colin", LastName = "Jaggs", Salary = 44000 }));
+			employees.Add(new EmployeeView((Employee)new Employee() { FirstName = "Adam", LastName = "Greenfield", Salary = 38000 }));
+			employees.Add(new EmployeeView((Employee)new Employee() { FirstName = "Dan", LastName = "Hutchinson", Salary = 34000 }));
+			employees.Add(new EmployeeView((Employee)new Employee() { FirstName = "Arek", LastName = "Gadecki", Salary = 27000 }));
+			employees.Add(new EmployeeView((Employee)new Employee() { FirstName = "Tony", LastName = "Cartridge", Salary = 8500 }));
+
+			// create new employee list view for the page
+			EmployeeListView vmEmpList = new EmployeeListView
+			{
+				Employees = employees.OrderBy(e => e.EmployeeName).ThenBy(e => e.Employee.Salary).ToList(),
+				UserName = "Admin"
+			};
+
+			ViewBag.TestVal = "Testing...";
+
+			return View(vmEmpList);
+		}
+
+		public ActionResult Page1()
+		{
+			return View();
+		}
+
+		public ActionResult Page2()
+		{
+			if (Session["RunOnce"] != null)
+			{
+				return View();
+			}
+			else
+			{
+				Session.Add("RunOnce", true);
+				return Content("You can only view this after your first visit.  Hit refresh to try again.");
+			}
+		}
+
+		public string GetString()
+		{
+			return "Hello World!  How many times have you seen this before?";
+		}
+	}
+}
---
+++ MVC5 Test Bed/Models/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5TestBed.Models
+{
+	#region Model
+
+	public class Employee
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Salary { get; set; }
+	}
+
+	#endregion
+
+	#region ModelView
+
+	public class EmployeeView
+	{
+		public Employee Employee { get; set; }
+		public string EmployeeName { get; }
+		public string Salary { get; }
+		public string SalaryColor { get; }
+
+		public EmployeeView(Employee emp)
+		{
+			this.Employee = emp;
+			this.EmployeeName = string.Format("{0} {1}", this.Employee.FirstName, this.Employee.LastName).Trim();
+			this.Salary = this.Employee.Salary.ToString("C");
+			this.SalaryColor = this.Employee.Salary >= 35000 ? "#ff0" : this.Employee.Salary <= 10000 ? "#f99" : "#0c0";
+		}
+	}
+
+	public class EmployeeListView
+	{
+		public List<EmployeeView> Employees { get; set; }
+		public string UserName { get; set; }
+	}
+
+	#endregion
+}
